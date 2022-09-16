94f98e7d1586ed083798f0ce9edcb2c9864c0fc1
Anderson Mancini <anderson@neotix.com.br>
2022-09-16 14:26:02 -0300

    First commit

--- Readme.md
+++ Readme.md
@@ -1,9 +1,29 @@
-# WebGi starter project
-A template for a vanilla(no ui-framework) project with webgi engine in typescript using parcel bundler.
+# Threejs + GSAP + WEGBi
+## 100% Free Course
+This is a template used in my fast course "building scrolable pages with ScrollTrigger and Threejs" for a vanilla (no ui-framework) project with webgi engine in typescript using parcel bundler.
 
-About webgi: [https://webgi.xyz/](https://webgi.xyz/)
+⚡️ Live Link: [http://drill-webgi-tutorial.vercel.app](http://drill-webgi-tutorial.vercel.app)
 
-## Running
+<a href="http://drill-webgi-tutorial.vercel.app"><img src="assets/images/preview.jpg" style="border: 1px solid black"></a>
+
+## Course content
+On my YouTube channel, you can find a **step by step** video on how to use this source code to build your own pages.
+
+- Chosing a model
+- Use WEBGi editor to create images 
+- Design the page using exported images from WEBGi
+- Setup the WEBGi boilerplate into VSCODE
+- Change the model
+- Create the html and CSS
+- Import GSAP and setup the library
+- Create the ScrollTrigger animation for the camera
+- Final adjustments
+
+<a href="https://www.youtube.com/watch?v=mpTZbJPYZas"><img src="assets/images/cover.png" style="border: 1px solid black"></a>
+
+[Click here](https://www.youtube.com/watch?v=mpTZbJPYZas) or on the image to visit the Free course on Youtube.
+
+## Getting started
 First install the dependencies:
 ```bash
 npm install
@@ -22,15 +42,10 @@ To build the project for production:
 npm run build
 ```
 
-## Updates
-Check the [webgi manual](https://webgi.xyz/docs/manual/#sdk-links) for the latest version.
-To use the different version:
-* Update the version number in `package.json` file for both `webgi` and `@types/webgi`.
-* Run `npm install` to update the dependencies.
-* Delete `.cache` folder created by parcel bundler: `rm -rf .cache`
-* Run `npm start` or `npm run build` to run or build the project.
-
 ## Documentation
+
+About webgi: [https://webgi.xyz/](https://webgi.xyz/)
+
 For the latest version and documentation: [WebGi Docs](https://webgi.xyz/docs/).
 
 ## License 
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,118 @@
-import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-
-    addBasePlugins,
-    ITexture,
-
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
-} from "webgi";
+import { ViewerApp, AssetManagerPlugin, GBufferPlugin, ProgressivePlugin, TonemapPlugin, SSRPlugin, SSAOPlugin, BloomPlugin } from "webgi";
 import "./styles.css";
+
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 async function setupViewer(){
 
-    // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
-        useRgbm: true,
+        useRgbm: false,
+        isAntialiased: true
     })
 
-    // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin)
+    const camera = viewer.scene.activeCamera
+    const position = camera.position
+    const target = camera.target
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
-
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(BloomPlugin)
 
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+    await manager.addFromPath("./assets/drill3.glb")
 
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
 
+    function setupScrollanimation(){
+
+        const tl = gsap.timeline()
+    
+        // FIRST SECTION
+    
+        tl
+        .to(position, {x: 1.56, y: -2.26, z: -3.85,
+            scrollTrigger: {
+                trigger: ".second", 
+                start:"top bottom",
+                end: "top top", scrub: true,
+                immediateRender: false
+        }, onUpdate})
+
+        .to(".section--one--container", { xPercent:'-150' , opacity:0,
+            scrollTrigger: {
+                trigger: ".second", 
+                start:"top bottom",
+                end: "top 80%", scrub: 1,
+                immediateRender: false
+        }})
+        .to(target, {x: -1.37, y: 1.99 , z: -0.37,
+            scrollTrigger: {
+                trigger: ".second", 
+                start:"top bottom",
+                end: "top top", scrub: true,
+                immediateRender: false
+        }})
+
+        // LAST SECTION
+        
+        .to(position, {x: -3.4, y: 9.6, z: 1.71,
+            scrollTrigger: {
+                trigger: ".third", 
+                start:"top bottom",
+                end: "top top", scrub: true,
+                immediateRender: false
+        }, onUpdate})
+
+        .to(target, {x: -1.5, y: 2.13 , z: -0.4,
+            scrollTrigger: {
+                trigger: ".third", 
+                start:"top bottom",
+                end: "top top", scrub: true,
+                immediateRender: false
+        }})
+        
+    }
+
+    setupScrollanimation()
+
+    // WEBGI UPDATE
+    let needsUpdate = true;
+
+    function onUpdate() {
+        needsUpdate = true
+        viewer.renderer.resetShadows()
+        viewer.setDirty()
+    }
+
+    viewer.addEventListener('preFrame', () =>{
+        if(needsUpdate){
+            camera.positionUpdated(true)
+            camera.targetUpdated(true)
+            needsUpdate = false
+        }
+    })
+
+    // KNOW MORE EVENT
+	document.querySelector('.button--hero')?.addEventListener('click', () => {
+		const element = document.querySelector('.second')
+		window.scrollTo({ top: element?.getBoundingClientRect().top, left: 0, behavior: 'smooth' })
+	})
+
+	// SCROLL TO TOP
+	document.querySelectorAll('.button--footer')?.forEach(item => {
+		item.addEventListener('click', () => {
+			window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+		})
+	})
 
 }
 
